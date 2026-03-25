35c449e504ed681599850cc52f1ecaac189e727e
jaswanth-33399 <jaswanth2500.t@gmail.com>
2026-03-25 12:58:28 +0530

    Time: 5 ms (47.55%), Space: 161.7 MB (41.96%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -2,35 +2,19 @@ class Solution
 {
     public boolean canPartitionGrid(int[][] grid) 
     {
+        long sum = 0;
+        for(int i=0;i<grid.length;i++) for(int j=0;j<grid[0].length;j++) sum += grid[i][j];
         long row_sum = 0;
-        List<Long> row = new ArrayList<>();
         long col_sum = 0;
-        List<Long> col = new ArrayList<>();
-        for(int i=0;i<grid.length;i++)
+        for(int i=0;i<grid.length-1;i++)
         {
-            long sum = 0;
-            for(int j=0;j<grid[0].length;j++) sum += grid[i][j];
-            row_sum += sum;
-            row.add(sum);
+            for(int j=0;j<grid[0].length;j++) row_sum += grid[i][j];
+            if(row_sum==(sum-row_sum)) return true;
         }
-        for(int i=0;i<grid[0].length;i++)
+        for(int i=0;i<grid[0].length-1;i++)
         {
-            long sum = 0;
-            for(int j=0;j<grid.length;j++) sum += grid[j][i];
-            col_sum += sum;
-            col.add(sum);
-        }
-        long sum_row = 0;
-        long sum_col = 0;
-        for(int i=0;i<row.size();i++)
-        {
-            sum_row += row.get(i);
-            if(sum_row==(row_sum-sum_row)) return true;
-        }
-        for(int i=0;i<col.size();i++)
-        {
-            sum_col += col.get(i);
-            if(sum_col==(col_sum-sum_col)) return true;
+            for(int j=0;j<grid.length;j++) col_sum += grid[j][i];
+            if(col_sum==(sum-col_sum)) return true;
         }
         return false;
     }
