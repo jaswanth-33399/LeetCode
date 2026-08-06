5e12fdb9efe4c8f64ea6534e7c76ddfa4f897cee
jaswanth-33399 <jaswanth2500.t@gmail.com>
2026-08-06 09:52:15 +0530

    Time: 1 ms (28.86%), Space: 19.2 MB (73.98%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def digcount(self,n):
+        prod=1
+        while (n>0) :
+            digit=n%10
+            prod*=digit
+            n//=10
+        return prod
+    def smallestNumber(self, n: int, t: int) -> int:
+            if ((self.digcount(n)) % t == 0):
+                return n
+            else :
+                for i in range(n,10**18):
+                    if (self.digcount(i)%t ==0 ):
+                        return i
+            return -1
